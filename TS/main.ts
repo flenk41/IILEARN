bd92aeec936ec84a313cd4fc3b9e8853e6f95348
flenk41 <efimoff.kirill2018@yandex.ru>
2026-08-12 13:45:34 +0300

    2

---
+++ TS/main.ts
@@ -0,0 +1,14 @@
+const MYname: string = "Name";
+const Years: number = 22;
+const Status: string = "Active";
+
+let balance: number = 5000;
+balance = 4500;
+
+let gamename = "FIFA26";
+let gameSell = 25;
+let gameStatus = "Active";
+
+const userAge: number = "21"; // записываем как строку
+let gameName: string = 100; // тут записываем как число
+const isOnline: boolean = "true"; // bool это либо 0 либо 1 или true and false
