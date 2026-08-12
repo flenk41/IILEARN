55eb291640b6ab9bf0c4887b052c9829725118df
flenk41 <efimoff.kirill2018@yandex.ru>
2026-08-12 15:53:55 +0300

    4

--- TS/main.ts
+++ TS/main.ts
@@ -1,51 +1,26 @@
-let gameName: string = "GOD";
-let pricegame: number = 200;
-let Userbalance: number = 100;
-let isAvalible: boolean = true;
+// 1 задача
 
-if (Userbalance >= pricegame) {
-    console.log("Покупка разрешена");
-}
-else if (isAvalible !== true) {
-    console.log("Игра не доступна");
-}
-else {
-    console.log("Недостаточно денег");
+function calculateBalance(dohod: number, razhod: number): number {
+    return dohod - razhod;
 }
 
+function canBuy(balance: number, price: number, isAvalible: boolean): boolean {
 
-// 3 задача
-const username: string = "Flenk";
-let balance: number = 5000;
-const isOnline: boolean = true;
-
-let age: number = 21;
-age = 22;
-
-const price: number = 1000;
-const usBalance: number = 500;
-
-if (usBalance >= price) {
-    console.log("Денег достаточно");
 }
 
-// 4 задача
+function formatTransaction(NameTrans: string, summ: number, other: string): string {
+    return NameTrans;
+}
 
-let score: number = 50;
+const summ = (number: number, numbers: number): number => {
+    return number + numbers;
+};
 
-if (score < 100 && score > 90) {
-    console.log("Отлично");
+const minus = (number: number, numbers: number): number => {
+    return number - numbers;
+};
+
+function calculet(callbackminus: (onenumber: number, twonumber: number) => number, callbackplus: (number1: number, number2: number) => number) {
+
 }
-else if (score > 70 && score < 89) {
-    console.log("Хорошо");
-}
-else if (score > 50 && score < 69) {
-    console.log("Зачет");
-}
-else if (score < 50) {
-    console.log("Не зачет");
-}
-else {
-    console.log("Некорректный результат");
-}
-// 5 
+
