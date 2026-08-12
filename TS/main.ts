2b4a48684386c4f7168cc1fd7029530b162036fe
flenk41 <efimoff.kirill2018@yandex.ru>
2026-08-12 22:40:30 +0300

    6

--- TS/main.ts
+++ TS/main.ts
@@ -68,3 +68,20 @@ console.log(getFinalPrice(item));
 const item2: Product = { name: "ПК", price: 65000, discount: 15 };
 console.log(getFinalPrice(item2));
 
+//4
+
+interface Player {
+    readonly id: number;
+    nickname: string;
+    level?: number;
+    email?: string;
+}
+
+const player: Player = {
+    id: 1,
+    nickname: "Flenk"
+};
+
+player.id; // можем только читать
+player.level = 10;
+player.email = "nasdasd_23123@mail.ru";
