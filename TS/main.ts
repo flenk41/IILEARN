80f873e46e3f77e387bcdd38735d29c396feba5e
flenk41 <efimoff.kirill2018@yandex.ru>
2026-08-12 23:25:14 +0300

    8

--- TS/main.ts
+++ TS/main.ts
@@ -1,87 +1,43 @@
-type Game = {
-    name: string;
-    price: number;
-    isAvalible: boolean;
-};
-
-const FirstGame: Game = {
-    name: "GoodOFWar",
-    price: 100,
-    isAvalible: true
-};
-
-const TwoGame: Game = {
-    name: "CSGO",
-    price: 300,
-    isAvalible: false
-};
-
-console.log(FirstGame, TwoGame);
-
-
+// 1
+function formatUserID(num: number | string) {
+    if (typeof num === "number") {
+        return num;
+    }
+    else if (typeof num === "string") {
+        return num.toUpperCase();
+    }
+}
 // 2
-interface Transaction {
-    id: number;
-    name: string;
-    amount: number;
-    category: string;
+type TransactionType = "income" | "expense";
+
+function describeTransaction(trans: TransactionType, sum: number): string {
+    return trans === "income" ? 'Доход: ${sum}' : 'Расход: ${sum}';
 }
 
-const trans: Transaction[] = [
-    {
-        id: 1,
-        name: "Покупка",
-        amount: 1500,
-        category: "Доход"
-    },
-    {
-        id: 2,
-        name: "Покупка",
-        amount: 4500,
-        category: "Доход"
+//3
+
+function processValue(unk: unknown) {
+    if (typeof unk === "string") {
+        return unk.length;   // ✅ TypeScript знает, что unk — строка
     }
-];
-
-for (let num of trans) {
-    console.log(num);
-}
-
-// 3
-
-interface Product {
-    name: string;
-    price: number;
-    discount?: number;
-}
-
-function getFinalPrice(prod: Product): number {
-    if (prod.discount !== 0 && prod.discount !== undefined) {
-        return prod.price * (1 - prod.discount / 100);
+    if (typeof unk === "number") {
+        return unk * 2;      // ✅ TypeScript знает, что unk — число
     }
-    else {
-        return prod.price;
+    if (typeof unk === "boolean") {
+        return unk;          // ✅ TypeScript знает, что unk — булево
+    }
+    return "Неизвестный тип";
+}
+
+// 4
+
+type PaymentStatus = "pending" | "paid" | "failed";
+
+function getPaymentMessage(PayStat: PaymentStatus) {
+    switch (PayStat) {
+        case "pending": return "pending";
+        case "paid": return "paid";
+        case "failed": return "paid";
+        default: const _exit: never = PayStat; return _exit;
     }
 }
-
-const item: Product = { name: "Ноутбук", price: 5000 };
-console.log(getFinalPrice(item));
-const item2: Product = { name: "ПК", price: 65000, discount: 15 };
-console.log(getFinalPrice(item2));
-
-//4
-
-interface Player {
-    readonly id: number;
-    nickname: string;
-    level?: number;
-    email?: string;
-}
-
-const player: Player = {
-    id: 1,
-    nickname: "Flenk"
-};
-
-player.id; // можем только читать
-player.level = 10;
-player.email = "nasdasd_23123@mail.ru";
