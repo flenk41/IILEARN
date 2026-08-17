4a45ba4af2f06a858e3749ffb565d372b2e7e4d4
flenk41 <efimoff.kirill2018@yandex.ru>
2026-08-17 07:33:15 +0300

    7

--- TS/main.ts
+++ TS/main.ts
@@ -1,43 +1,104 @@
 // 1
-function formatUserID(num: number | string) {
-    if (typeof num === "number") {
-        return num;
+class Player { 
+    // Поля
+    readonly id: number;
+    public nickname: string;
+    private level: number;
+
+    //Конструктор
+    constructor(id:number,nickname:string,level:number) { 
+        this.id = id;
+        this.nickname = nickname;
+        this.level = level;
     }
-    else if (typeof num === "string") {
-        return num.toUpperCase();
+    // методы
+    increaseLevel(): void{ 
+        this.level++;
+    }
+
+    getLevel() { 
+        return this.level;
     }
 }
-// 2
-type TransactionType = "income" | "expense";
 
-function describeTransaction(trans: TransactionType, sum: number): string {
-    return trans === "income" ? 'Доход: ${sum}' : 'Расход: ${sum}';
+const player = new Player(1, "Flenk", 1);
+
+console.log(player.getLevel()); // 1
+player.increaseLevel();
+console.log(player.getLevel()); // 2
+
+//
+
+class BankAccount { 
+    constructor(
+        readonly accountID: number,
+        private balance: number
+    ) {}
+
+    deposit(amount: number): boolean { 
+        if (amount > 0) { 
+            this.balance += amount;
+            return true;
+        }
+        return false;
+    }
+    withdraw(amount: number): boolean { 
+        if (amount > 0 && amount <= this.balance) {
+            this.balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
 }
 
-//3
+const AccountBank = new BankAccount(1, 100);
+console.log(AccountBank.deposit(200));
+console.log(AccountBank.withdraw(300))
 
-function processValue(unk: unknown) {
-    if (typeof unk === "string") {
-        return unk.length;   // ✅ TypeScript знает, что unk — строка
+
+// 3
+
+interface Priceable { 
+    name: string;
+    price: number;
+    descount?: number;
+
+    getFinalPrice(): number;
+}
+
+class Product implements Priceable { 
+    name: string;
+    price: number;
+    descount?: number;
+
+    constructor(name: string, price: number,descount?:number) {
+        this.name = name;
+        this.price = price;
+        this.descount = descount || 0;
     }
-    if (typeof unk === "number") {
-        return unk * 2;      // ✅ TypeScript знает, что unk — число
+
+    getFinalPrice(): number { 
+        return this.price * (1 - this.descount / 100);
     }
-    if (typeof unk === "boolean") {
-        return unk;          // ✅ TypeScript знает, что unk — булево
-    }
-    return "Неизвестный тип";
 }
 
 // 4
 
-type PaymentStatus = "pending" | "paid" | "failed";
+class User { 
+    id: number;
+    private password: string;
 
-function getPaymentMessage(PayStat: PaymentStatus) {
-    switch (PayStat) {
-        case "pending": return "pending";
-        case "paid": return "paid";
-        case "failed": return "paid";
-        default: const _exit: never = PayStat; return _exit;
+    constructor(id: number, password: string) { 
+        this.id = id;
+        this.password = password;
+    }
+    getPassword(): string { 
+        return this.password;
     }
 }
+
+const user = new User(1, "secret");
+
+user.id = 2;
+console.log(user.getPassword());
